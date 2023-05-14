a2355cc7edb8fcc85692d2998fb21a936cc057ec
Naohiro Yoshida <naohiro.yoshida@datachain.jp>
2023-05-14 11:46:18 +0900

    fix test
    Signed-off-by: Naohiro Yoshida <naohiro.yoshida@datachain.jp>

--- e2e/contracts/contracts/ibc/lightclients/parlia/v1/parlia.sol
+++ e2e/contracts/contracts/ibc/lightclients/parlia/v1/parlia.sol
@@ -917,14 +917,13 @@ library IbcLightclientsParliaV1ETHHeader {
 }
 //library IbcLightclientsParliaV1ETHHeader
 
-library IbcLightclientsParliaV1Header {
+library IbcLightclientsParliaV1ValidatorSet {
 
 
   //struct definition
   struct Data {
-    IbcLightclientsParliaV1ETHHeader.Data[] headers;
-    IbcCoreClientV1Height.Data trusted_height;
-    bytes account_proof;
+    IbcCoreClientV1Height.Data epoch_height;
+    bytes[] validators;
   }
 
   // Decoder section
@@ -964,7 +963,333 @@ library IbcLightclientsParliaV1Header {
     returns (Data memory, uint)
   {
     Data memory r;
-    uint[4] memory counters;
+    uint[3] memory counters;
+    uint256 fieldId;
+    ProtoBufRuntime.WireType wireType;
+    uint256 bytesRead;
+    uint256 offset = p;
+    uint256 pointer = p;
+    while (pointer < offset + sz) {
+      (fieldId, wireType, bytesRead) = ProtoBufRuntime._decode_key(pointer, bs);
+      pointer += bytesRead;
+      if (fieldId == 1) {
+        pointer += _read_epoch_height(pointer, bs, r);
+      } else
+      if (fieldId == 2) {
+        pointer += _read_unpacked_repeated_validators(pointer, bs, nil(), counters);
+      } else
+      {
+        pointer += ProtoBufRuntime._skip_field_decode(wireType, pointer, bs);
+      }
+
+    }
+    pointer = offset;
+    if (counters[2] > 0) {
+      require(r.validators.length == 0);
+      r.validators = new bytes[](counters[2]);
+    }
+
+    while (pointer < offset + sz) {
+      (fieldId, wireType, bytesRead) = ProtoBufRuntime._decode_key(pointer, bs);
+      pointer += bytesRead;
+      if (fieldId == 2) {
+        pointer += _read_unpacked_repeated_validators(pointer, bs, r, counters);
+      } else
+      {
+        pointer += ProtoBufRuntime._skip_field_decode(wireType, pointer, bs);
+      }
+    }
+    return (r, sz);
+  }
+
+  // field readers
+
+  /**
+   * @dev The decoder for reading a field
+   * @param p The offset of bytes array to start decode
+   * @param bs The bytes array to be decoded
+   * @param r The in-memory struct
+   * @return The number of bytes decoded
+   */
+  function _read_epoch_height(
+    uint256 p,
+    bytes memory bs,
+    Data memory r
+  ) internal pure returns (uint) {
+    (IbcCoreClientV1Height.Data memory x, uint256 sz) = _decode_IbcCoreClientV1Height(p, bs);
+    r.epoch_height = x;
+    return sz;
+  }
+
+  /**
+   * @dev The decoder for reading a field
+   * @param p The offset of bytes array to start decode
+   * @param bs The bytes array to be decoded
+   * @param r The in-memory struct
+   * @param counters The counters for repeated fields
+   * @return The number of bytes decoded
+   */
+  function _read_unpacked_repeated_validators(
+    uint256 p,
+    bytes memory bs,
+    Data memory r,
+    uint[3] memory counters
+  ) internal pure returns (uint) {
+    /**
+     * if `r` is NULL, then only counting the number of fields.
+     */
+    (bytes memory x, uint256 sz) = ProtoBufRuntime._decode_bytes(p, bs);
+    if (isNil(r)) {
+      counters[2] += 1;
+    } else {
+      r.validators[r.validators.length - counters[2]] = x;
+      counters[2] -= 1;
+    }
+    return sz;
+  }
+
+  // struct decoder
+  /**
+   * @dev The decoder for reading a inner struct field
+   * @param p The offset of bytes array to start decode
+   * @param bs The bytes array to be decoded
+   * @return The decoded inner-struct
+   * @return The number of bytes used to decode
+   */
+  function _decode_IbcCoreClientV1Height(uint256 p, bytes memory bs)
+    internal
+    pure
+    returns (IbcCoreClientV1Height.Data memory, uint)
+  {
+    uint256 pointer = p;
+    (uint256 sz, uint256 bytesRead) = ProtoBufRuntime._decode_varint(pointer, bs);
+    pointer += bytesRead;
+    (IbcCoreClientV1Height.Data memory r, ) = IbcCoreClientV1Height._decode(pointer, bs, sz);
+    return (r, sz + bytesRead);
+  }
+
+
+  // Encoder section
+
+  /**
+   * @dev The main encoder for memory
+   * @param r The struct to be encoded
+   * @return The encoded byte array
+   */
+  function encode(Data memory r) internal pure returns (bytes memory) {
+    bytes memory bs = new bytes(_estimate(r));
+    uint256 sz = _encode(r, 32, bs);
+    assembly {
+      mstore(bs, sz)
+    }
+    return bs;
+  }
+  // inner encoder
+
+  /**
+   * @dev The encoder for internal usage
+   * @param r The struct to be encoded
+   * @param p The offset of bytes array to start decode
+   * @param bs The bytes array to be decoded
+   * @return The number of bytes encoded
+   */
+  function _encode(Data memory r, uint256 p, bytes memory bs)
+    internal
+    pure
+    returns (uint)
+  {
+    uint256 offset = p;
+    uint256 pointer = p;
+    uint256 i;
+    
+    pointer += ProtoBufRuntime._encode_key(
+      1,
+      ProtoBufRuntime.WireType.LengthDelim,
+      pointer,
+      bs
+    );
+    pointer += IbcCoreClientV1Height._encode_nested(r.epoch_height, pointer, bs);
+    
+    if (r.validators.length != 0) {
+    for(i = 0; i < r.validators.length; i++) {
+      pointer += ProtoBufRuntime._encode_key(
+        2,
+        ProtoBufRuntime.WireType.LengthDelim,
+        pointer,
+        bs)
+      ;
+      pointer += ProtoBufRuntime._encode_bytes(r.validators[i], pointer, bs);
+    }
+    }
+    return pointer - offset;
+  }
+  // nested encoder
+
+  /**
+   * @dev The encoder for inner struct
+   * @param r The struct to be encoded
+   * @param p The offset of bytes array to start decode
+   * @param bs The bytes array to be decoded
+   * @return The number of bytes encoded
+   */
+  function _encode_nested(Data memory r, uint256 p, bytes memory bs)
+    internal
+    pure
+    returns (uint)
+  {
+    /**
+     * First encoded `r` into a temporary array, and encode the actual size used.
+     * Then copy the temporary array into `bs`.
+     */
+    uint256 offset = p;
+    uint256 pointer = p;
+    bytes memory tmp = new bytes(_estimate(r));
+    uint256 tmpAddr = ProtoBufRuntime.getMemoryAddress(tmp);
+    uint256 bsAddr = ProtoBufRuntime.getMemoryAddress(bs);
+    uint256 size = _encode(r, 32, tmp);
+    pointer += ProtoBufRuntime._encode_varint(size, pointer, bs);
+    ProtoBufRuntime.copyBytes(tmpAddr + 32, bsAddr + pointer, size);
+    pointer += size;
+    delete tmp;
+    return pointer - offset;
+  }
+  // estimator
+
+  /**
+   * @dev The estimator for a struct
+   * @param r The struct to be encoded
+   * @return The number of bytes encoded in estimation
+   */
+  function _estimate(
+    Data memory r
+  ) internal pure returns (uint) {
+    uint256 e;uint256 i;
+    e += 1 + ProtoBufRuntime._sz_lendelim(IbcCoreClientV1Height._estimate(r.epoch_height));
+    for(i = 0; i < r.validators.length; i++) {
+      e += 1 + ProtoBufRuntime._sz_lendelim(r.validators[i].length);
+    }
+    return e;
+  }
+  // empty checker
+
+  function _empty(
+    Data memory r
+  ) internal pure returns (bool) {
+    
+  if (r.validators.length != 0) {
+    return false;
+  }
+
+    return true;
+  }
+
+
+  //store function
+  /**
+   * @dev Store in-memory struct to storage
+   * @param input The in-memory struct
+   * @param output The in-storage struct
+   */
+  function store(Data memory input, Data storage output) internal {
+    IbcCoreClientV1Height.store(input.epoch_height, output.epoch_height);
+    output.validators = input.validators;
+
+  }
+
+
+  //array helpers for Validators
+  /**
+   * @dev Add value to an array
+   * @param self The in-memory struct
+   * @param value The value to add
+   */
+  function addValidators(Data memory self, bytes memory value) internal pure {
+    /**
+     * First resize the array. Then add the new element to the end.
+     */
+    bytes[] memory tmp = new bytes[](self.validators.length + 1);
+    for (uint256 i = 0; i < self.validators.length; i++) {
+      tmp[i] = self.validators[i];
+    }
+    tmp[self.validators.length] = value;
+    self.validators = tmp;
+  }
+
+
+  //utility functions
+  /**
+   * @dev Return an empty struct
+   * @return r The empty struct
+   */
+  function nil() internal pure returns (Data memory r) {
+    assembly {
+      r := 0
+    }
+  }
+
+  /**
+   * @dev Test whether a struct is empty
+   * @param x The struct to be tested
+   * @return r True if it is empty
+   */
+  function isNil(Data memory x) internal pure returns (bool r) {
+    assembly {
+      r := iszero(x)
+    }
+  }
+}
+//library IbcLightclientsParliaV1ValidatorSet
+
+library IbcLightclientsParliaV1Header {
+
+
+  //struct definition
+  struct Data {
+    IbcLightclientsParliaV1ETHHeader.Data[] headers;
+    IbcCoreClientV1Height.Data trusted_height;
+    bytes account_proof;
+    IbcLightclientsParliaV1ValidatorSet.Data previous_validators;
+    IbcLightclientsParliaV1ValidatorSet.Data current_validators;
+  }
+
+  // Decoder section
+
+  /**
+   * @dev The main decoder for memory
+   * @param bs The bytes array to be decoded
+   * @return The decoded struct
+   */
+  function decode(bytes memory bs) internal pure returns (Data memory) {
+    (Data memory x, ) = _decode(32, bs, bs.length);
+    return x;
+  }
+
+  /**
+   * @dev The main decoder for storage
+   * @param self The in-storage struct
+   * @param bs The bytes array to be decoded
+   */
+  function decode(Data storage self, bytes memory bs) internal {
+    (Data memory x, ) = _decode(32, bs, bs.length);
+    store(x, self);
+  }
+  // inner decoder
+
+  /**
+   * @dev The decoder for internal usage
+   * @param p The offset of bytes array to start decode
+   * @param bs The bytes array to be decoded
+   * @param sz The number of bytes expected
+   * @return The decoded struct
+   * @return The number of bytes decoded
+   */
+  function _decode(uint256 p, bytes memory bs, uint256 sz)
+    internal
+    pure
+    returns (Data memory, uint)
+  {
+    Data memory r;
+    uint[6] memory counters;
     uint256 fieldId;
     ProtoBufRuntime.WireType wireType;
     uint256 bytesRead;
@@ -981,6 +1306,12 @@ library IbcLightclientsParliaV1Header {
       } else
       if (fieldId == 3) {
         pointer += _read_account_proof(pointer, bs, r);
+      } else
+      if (fieldId == 4) {
+        pointer += _read_previous_validators(pointer, bs, r);
+      } else
+      if (fieldId == 5) {
+        pointer += _read_current_validators(pointer, bs, r);
       } else
       {
         pointer += ProtoBufRuntime._skip_field_decode(wireType, pointer, bs);
@@ -1020,7 +1351,7 @@ library IbcLightclientsParliaV1Header {
     uint256 p,
     bytes memory bs,
     Data memory r,
-    uint[4] memory counters
+    uint[6] memory counters
   ) internal pure returns (uint) {
     /**
      * if `r` is NULL, then only counting the number of fields.
@@ -1069,6 +1400,40 @@ library IbcLightclientsParliaV1Header {
     return sz;
   }
 
+  /**
+   * @dev The decoder for reading a field
+   * @param p The offset of bytes array to start decode
+   * @param bs The bytes array to be decoded
+   * @param r The in-memory struct
+   * @return The number of bytes decoded
+   */
+  function _read_previous_validators(
+    uint256 p,
+    bytes memory bs,
+    Data memory r
+  ) internal pure returns (uint) {
+    (IbcLightclientsParliaV1ValidatorSet.Data memory x, uint256 sz) = _decode_IbcLightclientsParliaV1ValidatorSet(p, bs);
+    r.previous_validators = x;
+    return sz;
+  }
+
+  /**
+   * @dev The decoder for reading a field
+   * @param p The offset of bytes array to start decode
+   * @param bs The bytes array to be decoded
+   * @param r The in-memory struct
+   * @return The number of bytes decoded
+   */
+  function _read_current_validators(
+    uint256 p,
+    bytes memory bs,
+    Data memory r
+  ) internal pure returns (uint) {
+    (IbcLightclientsParliaV1ValidatorSet.Data memory x, uint256 sz) = _decode_IbcLightclientsParliaV1ValidatorSet(p, bs);
+    r.current_validators = x;
+    return sz;
+  }
+
   // struct decoder
   /**
    * @dev The decoder for reading a inner struct field
@@ -1105,6 +1470,25 @@ library IbcLightclientsParliaV1Header {
     (uint256 sz, uint256 bytesRead) = ProtoBufRuntime._decode_varint(pointer, bs);
     pointer += bytesRead;
     (IbcCoreClientV1Height.Data memory r, ) = IbcCoreClientV1Height._decode(pointer, bs, sz);
+    return (r, sz + bytesRead);
+  }
+
+  /**
+   * @dev The decoder for reading a inner struct field
+   * @param p The offset of bytes array to start decode
+   * @param bs The bytes array to be decoded
+   * @return The decoded inner-struct
+   * @return The number of bytes used to decode
+   */
+  function _decode_IbcLightclientsParliaV1ValidatorSet(uint256 p, bytes memory bs)
+    internal
+    pure
+    returns (IbcLightclientsParliaV1ValidatorSet.Data memory, uint)
+  {
+    uint256 pointer = p;
+    (uint256 sz, uint256 bytesRead) = ProtoBufRuntime._decode_varint(pointer, bs);
+    pointer += bytesRead;
+    (IbcLightclientsParliaV1ValidatorSet.Data memory r, ) = IbcLightclientsParliaV1ValidatorSet._decode(pointer, bs, sz);
     return (r, sz + bytesRead);
   }
 
@@ -1170,6 +1554,24 @@ library IbcLightclientsParliaV1Header {
     );
     pointer += ProtoBufRuntime._encode_bytes(r.account_proof, pointer, bs);
     }
+    
+    pointer += ProtoBufRuntime._encode_key(
+      4,
+      ProtoBufRuntime.WireType.LengthDelim,
+      pointer,
+      bs
+    );
+    pointer += IbcLightclientsParliaV1ValidatorSet._encode_nested(r.previous_validators, pointer, bs);
+    
+    
+    pointer += ProtoBufRuntime._encode_key(
+      5,
+      ProtoBufRuntime.WireType.LengthDelim,
+      pointer,
+      bs
+    );
+    pointer += IbcLightclientsParliaV1ValidatorSet._encode_nested(r.current_validators, pointer, bs);
+    
     return pointer - offset;
   }
   // nested encoder
@@ -1218,6 +1620,8 @@ library IbcLightclientsParliaV1Header {
     }
     e += 1 + ProtoBufRuntime._sz_lendelim(IbcCoreClientV1Height._estimate(r.trusted_height));
     e += 1 + ProtoBufRuntime._sz_lendelim(r.account_proof.length);
+    e += 1 + ProtoBufRuntime._sz_lendelim(IbcLightclientsParliaV1ValidatorSet._estimate(r.previous_validators));
+    e += 1 + ProtoBufRuntime._sz_lendelim(IbcLightclientsParliaV1ValidatorSet._estimate(r.current_validators));
     return e;
   }
   // empty checker
@@ -1252,6 +1656,8 @@ library IbcLightclientsParliaV1Header {
     
     IbcCoreClientV1Height.store(input.trusted_height, output.trusted_height);
     output.account_proof = input.account_proof;
+    IbcLightclientsParliaV1ValidatorSet.store(input.previous_validators, output.previous_validators);
+    IbcLightclientsParliaV1ValidatorSet.store(input.current_validators, output.current_validators);
 
   }
 
@@ -1306,7 +1712,7 @@ library IbcLightclientsParliaV1ConsensusState {
   struct Data {
     bytes state_root;
     uint64 timestamp;
-    bytes[] validator_set;
+    bytes validators_hash;
   }
 
   // Decoder section
@@ -1346,7 +1752,6 @@ library IbcLightclientsParliaV1ConsensusState {
     returns (Data memory, uint)
   {
     Data memory r;
-    uint[4] memory counters;
     uint256 fieldId;
     ProtoBufRuntime.WireType wireType;
     uint256 bytesRead;
@@ -1362,28 +1767,12 @@ library IbcLightclientsParliaV1ConsensusState {
         pointer += _read_timestamp(pointer, bs, r);
       } else
       if (fieldId == 3) {
-        pointer += _read_unpacked_repeated_validator_set(pointer, bs, nil(), counters);
+        pointer += _read_validators_hash(pointer, bs, r);
       } else
       {
         pointer += ProtoBufRuntime._skip_field_decode(wireType, pointer, bs);
       }
 
-    }
-    pointer = offset;
-    if (counters[3] > 0) {
-      require(r.validator_set.length == 0);
-      r.validator_set = new bytes[](counters[3]);
-    }
-
-    while (pointer < offset + sz) {
-      (fieldId, wireType, bytesRead) = ProtoBufRuntime._decode_key(pointer, bs);
-      pointer += bytesRead;
-      if (fieldId == 3) {
-        pointer += _read_unpacked_repeated_validator_set(pointer, bs, r, counters);
-      } else
-      {
-        pointer += ProtoBufRuntime._skip_field_decode(wireType, pointer, bs);
-      }
     }
     return (r, sz);
   }
@@ -1429,25 +1818,15 @@ library IbcLightclientsParliaV1ConsensusState {
    * @param p The offset of bytes array to start decode
    * @param bs The bytes array to be decoded
    * @param r The in-memory struct
-   * @param counters The counters for repeated fields
    * @return The number of bytes decoded
    */
-  function _read_unpacked_repeated_validator_set(
+  function _read_validators_hash(
     uint256 p,
     bytes memory bs,
-    Data memory r,
-    uint[4] memory counters
+    Data memory r
   ) internal pure returns (uint) {
-    /**
-     * if `r` is NULL, then only counting the number of fields.
-     */
     (bytes memory x, uint256 sz) = ProtoBufRuntime._decode_bytes(p, bs);
-    if (isNil(r)) {
-      counters[3] += 1;
-    } else {
-      r.validator_set[r.validator_set.length - counters[3]] = x;
-      counters[3] -= 1;
-    }
+    r.validators_hash = x;
     return sz;
   }
 
@@ -1483,7 +1862,7 @@ library IbcLightclientsParliaV1ConsensusState {
   {
     uint256 offset = p;
     uint256 pointer = p;
-    uint256 i;
+    
     if (r.state_root.length != 0) {
     pointer += ProtoBufRuntime._encode_key(
       1,
@@ -1502,16 +1881,14 @@ library IbcLightclientsParliaV1ConsensusState {
     );
     pointer += ProtoBufRuntime._encode_uint64(r.timestamp, pointer, bs);
     }
-    if (r.validator_set.length != 0) {
-    for(i = 0; i < r.validator_set.length; i++) {
-      pointer += ProtoBufRuntime._encode_key(
-        3,
-        ProtoBufRuntime.WireType.LengthDelim,
-        pointer,
-        bs)
-      ;
-      pointer += ProtoBufRuntime._encode_bytes(r.validator_set[i], pointer, bs);
-    }
+    if (r.validators_hash.length != 0) {
+    pointer += ProtoBufRuntime._encode_key(
+      3,
+      ProtoBufRuntime.WireType.LengthDelim,
+      pointer,
+      bs
+    );
+    pointer += ProtoBufRuntime._encode_bytes(r.validators_hash, pointer, bs);
     }
     return pointer - offset;
   }
@@ -1555,12 +1932,10 @@ library IbcLightclientsParliaV1ConsensusState {
   function _estimate(
     Data memory r
   ) internal pure returns (uint) {
-    uint256 e;uint256 i;
+    uint256 e;
     e += 1 + ProtoBufRuntime._sz_lendelim(r.state_root.length);
     e += 1 + ProtoBufRuntime._sz_uint64(r.timestamp);
-    for(i = 0; i < r.validator_set.length; i++) {
-      e += 1 + ProtoBufRuntime._sz_lendelim(r.validator_set[i].length);
-    }
+    e += 1 + ProtoBufRuntime._sz_lendelim(r.validators_hash.length);
     return e;
   }
   // empty checker
@@ -1577,7 +1952,7 @@ library IbcLightclientsParliaV1ConsensusState {
     return false;
   }
 
-  if (r.validator_set.length != 0) {
+  if (r.validators_hash.length != 0) {
     return false;
   }
 
@@ -1594,28 +1969,10 @@ library IbcLightclientsParliaV1ConsensusState {
   function store(Data memory input, Data storage output) internal {
     output.state_root = input.state_root;
     output.timestamp = input.timestamp;
-    output.validator_set = input.validator_set;
+    output.validators_hash = input.validators_hash;
 
   }
 
-
-  //array helpers for ValidatorSet
-  /**
-   * @dev Add value to an array
-   * @param self The in-memory struct
-   * @param value The value to add
-   */
-  function addValidatorSet(Data memory self, bytes memory value) internal pure {
-    /**
-     * First resize the array. Then add the new element to the end.
-     */
-    bytes[] memory tmp = new bytes[](self.validator_set.length + 1);
-    for (uint256 i = 0; i < self.validator_set.length; i++) {
-      tmp[i] = self.validator_set[i];
-    }
-    tmp[self.validator_set.length] = value;
-    self.validator_set = tmp;
-  }
 
 
   //utility functions
