5b801d14a34933d002267349ba9b545a3a1ef01b
Naohiro Yoshida <naohiro.yoshida@datachain.jp>
2023-06-24 15:50:51 +0900

    use cobra to create testdata
    Signed-off-by: Naohiro Yoshida <naohiro.yoshida@datachain.jp>

--- tool/testdata/internal/create_misbehavior.go
+++ tool/testdata/internal/create_misbehavior.go
@@ -1,4 +1,4 @@
-package main
+package internal
 
 import (
 	"fmt"
@@ -6,6 +6,7 @@ import (
 	"github.com/datachainlab/ibc-parlia-relay/module"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/hyperledger-labs/yui-ibc-solidity/pkg/relay/ethereum"
+	"github.com/spf13/cobra"
 	"log"
 )
 
@@ -15,33 +16,49 @@ const (
 	IbcAddress  = "0x702E40245797c5a2108A566b3CE2Bf14Bc6aF841"
 )
 
+func CreateMisbehavior() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "misbehavior",
+		Short: "Create testdata for misbehavior. ",
+	}
+	cmd.AddCommand(localNet())
+	return cmd
+}
+
 // Launch local net before execute.
 //   - make chain
 //     -> Change NUMS_OF_VALIDATOR to 1 in docker-compose.yml
 //     -> Use e2e/chains/bsc/validators/keystore/bsc-validator1 for each chain's validator
 //   - make contracts
-func main() {
-	chainID := int64(9999)
-	height := uint64(200)
-	header1, err := getHeader(chainID, 8645, height)
-	if err != nil {
-		log.Panic(err)
-	}
-	header2, err := getHeader(chainID, 8545, height)
-	if err != nil {
-		log.Panic(err)
-	}
-	clientId := "xx-parlia-1"
-	misbehavior := module.Misbehaviour{
-		ClientId: clientId,
-		Header_1: header1,
-		Header_2: header2,
-	}
+func localNet() *cobra.Command {
+	return &cobra.Command{
+		Use:   "local",
+		Short: "create misbehavior testdata with local net",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			chainID := int64(9999)
+			height := uint64(200)
+			header1, err := getHeader(chainID, 8645, height)
+			if err != nil {
+				log.Panic(err)
+			}
+			header2, err := getHeader(chainID, 8545, height)
+			if err != nil {
+				log.Panic(err)
+			}
+			clientId := "xx-parlia-1"
+			misbehavior := module.Misbehaviour{
+				ClientId: clientId,
+				Header_1: header1,
+				Header_2: header2,
+			}
 
-	// print hex for lcp-parlia test
-	pack, _ := types.PackMisbehaviour(&misbehavior)
-	marshal, _ := pack.Marshal()
-	log.Println(common.Bytes2Hex(marshal))
+			// print hex for lcp-parlia test
+			pack, _ := types.PackMisbehaviour(&misbehavior)
+			marshal, _ := pack.Marshal()
+			log.Println(common.Bytes2Hex(marshal))
+			return nil
+		},
+	}
 }
 
 func getHeader(chainID int64, port int64, latestBlockNumber uint64) (*module.Header, error) {
