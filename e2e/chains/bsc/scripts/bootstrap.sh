01a284fa31363822f35bbdea3033f56c7fd1ffa5
Naohiro Yoshida <naohiro.yoshida@datachain.jp>
2025-03-17 15:16:23 +0900

    change epoch length
    Signed-off-by: Naohiro Yoshida <naohiro.yoshida@datachain.jp>

--- e2e/chains/bsc/scripts/bootstrap.sh
+++ e2e/chains/bsc/scripts/bootstrap.sh
@@ -35,10 +35,6 @@ function init_validator() {
 function generate_genesis() {
   INIT_HOLDER_ADDRESSES=$(ls ${workspace}/init-holders | tr '\n' ',')
   INIT_HOLDER_ADDRESSES=${INIT_HOLDER_ADDRESSES/%,/}
-  echo "blocks per epoch = ${BLOCKS_PER_EPOCH}"
-
-  echo "replace genesis-template.template"
-  sed "s/{{BLOCKS_PER_EPOCH}}/${BLOCKS_PER_EPOCH}/g" genesis-template.template >genesis-template.json
 
   echo "replace init_holders.template"
   sed "s/{{INIT_HOLDER_ADDRESSES}}/${INIT_HOLDER_ADDRESSES}/g" scripts/init_holders.template | sed "s/{{INIT_HOLDER_BALANCE}}/${INIT_HOLDER_BALANCE}/g" >scripts/init_holders.js
@@ -49,7 +45,6 @@ function generate_genesis() {
 
   echo "replace generate.py"
   sed "s/dev_chain_id: int = 714/dev_chain_id: int = ${BSC_CHAIN_ID}/g" scripts/generate.py > scripts/generate.py.out
-  sed "s/epoch: str = \"200\"/epoch: str = \"${BLOCKS_PER_EPOCH}\"/g" scripts/generate.py.out > scripts/generate.py
 
   echo "start generate validators"
   node scripts/generate-validator.js
