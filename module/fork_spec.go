5ecb5178ab172d0efe662b20344532cb17fd4019
Naohiro Yoshida <naohiro.yoshida@datachain.jp>
2025-03-20 15:01:44 +0900

    add epoch length
    Signed-off-by: Naohiro Yoshida <naohiro.yoshida@datachain.jp>

--- module/fork_spec.go
+++ module/fork_spec.go
@@ -1,5 +1,10 @@
 package module
 
+import (
+	"os"
+	"strconv"
+)
+
 type Network string
 
 const (
@@ -9,12 +14,31 @@ const (
 )
 
 func GetForkParameters(network Network) []*ForkSpec {
+
 	switch network {
 	case Localnet:
+
+		localLorentzHFTimestamp := os.Getenv("LOCAL_LORENTZ_HF_TIMESTAMP")
+		localLorentzHFTimestampInt := uint64(1)
+		if localLorentzHFTimestamp != "" {
+			result, err := strconv.Atoi(localLorentzHFTimestamp)
+			if err != nil {
+				panic(err)
+			}
+			localLorentzHFTimestampInt = uint64(result)
+		}
 		return []*ForkSpec{
+			// Pascal HF
 			{
 				HeightOrTimestamp:         &ForkSpec_Timestamp{Timestamp: 0},
 				AdditionalHeaderItemCount: 1,
+				EpochLength:               200,
+			},
+			// Lorentz HF
+			{
+				HeightOrTimestamp:         &ForkSpec_Timestamp{Timestamp: localLorentzHFTimestampInt},
+				AdditionalHeaderItemCount: 1,
+				EpochLength:               500,
 			},
 		}
 	case Testnet:
@@ -22,6 +46,12 @@ func GetForkParameters(network Network) []*ForkSpec {
 			{
 				HeightOrTimestamp:         &ForkSpec_Timestamp{Timestamp: 0},
 				AdditionalHeaderItemCount: 1,
+				EpochLength:               200,
+			},
+			{
+				HeightOrTimestamp:         &ForkSpec_Timestamp{Timestamp: 1},
+				AdditionalHeaderItemCount: 1,
+				EpochLength:               500,
 			},
 		}
 	case Mainnet:
@@ -29,6 +59,12 @@ func GetForkParameters(network Network) []*ForkSpec {
 			{
 				HeightOrTimestamp:         &ForkSpec_Timestamp{Timestamp: 0},
 				AdditionalHeaderItemCount: 1,
+				EpochLength:               200,
+			},
+			{
+				HeightOrTimestamp:         &ForkSpec_Timestamp{Timestamp: 1},
+				AdditionalHeaderItemCount: 1,
+				EpochLength:               500,
 			},
 		}
 	}
