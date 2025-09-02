89539851b3434aa0bb9b446678da3be340c8d909
Naohiro Yoshida <naohiro.yoshida@datachain.jp>
2025-09-02 10:30:35 +0900

    fix test

--- e2e/contracts/scripts/recv.js
+++ e2e/contracts/scripts/recv.js
@@ -3,13 +3,14 @@ const util = require("./util");
 async function main() {
   const accounts = await hre.ethers.getSigners();
   const bob = accounts[1]
-
   try {
 
+    const token= await util.readContract("ERC20Token");
+    const baseDenom = token.target.toLowerCase();
     const transfer = await util.readContract("ICS20Transfer");
-    const bobAmount = await transfer.balanceOf(bob, `transfer/channel-0/simple_erc_20_token_for_test`)
+    const bobAmount = await transfer.balanceOf(bob, `transfer/${util.config.channel}/${baseDenom}`)
     console.log("received = ", bobAmount.toString())
-    if (parseInt(bobAmount.toString(), 10) !== 20) {
+    if (parseInt(bobAmount.toString(), 10) !== util.config.amount) {
       throw new Error("bob amount error");
     }
 
--- e2e/contracts/scripts/send.js
+++ e2e/contracts/scripts/send.js
@@ -4,39 +4,36 @@ async function main() {
   const accounts = await hre.ethers.getSigners();
   const alice = accounts[0].address;
   const bob = accounts[1].address;
-
-  const channel = "channel-0";
-  const timeoutHeight = [
-    [BigInt(1), BigInt(10000000)],
-    BigInt(0)
-  ];
-  const sendingAmount = 20
-
+  const timeoutHeight = [[1, 10000000],0];
   try {
     const token = await util.readContract("ERC20Token");
-    const before= await token.balanceOf(alice)
-    console.log("before = ", before.toString())
     const transfer = await util.readContract("ICS20Transfer");
 
+    const before= await token.balanceOf(alice)
+    console.log("before = ", before.toString())
+
+    const escrowBefore= await token.balanceOf(transfer.target)
+    console.log("escrow before = ", escrowBefore.toString())
+
     // Deposit and SendTransfer
-    await token.approve(transfer.target, sendingAmount, { from: alice }).then(tx => tx.wait());
-    const depositResult = await transfer.depositSendTransfer(channel, token.target, sendingAmount, bob, timeoutHeight, {
+    await token.approve(transfer.target, util.config.amount, { from: alice }).then(tx => tx.wait());
+    const depositResult = await transfer.depositSendTransfer(util.config.channel, token.target, util.config.amount, bob, timeoutHeight, {
       from: alice
     })
     const depositReceipt = await depositResult.wait()
-    console.log("deposit success", depositReceipt.hash);
+    console.log("depositSendTransfer success", depositReceipt.hash);
 
     // Check reduced amount
     const after= await token.balanceOf(alice)
     console.log("after = ", after.toString())
-    if (parseInt(after.toString(), 10) !== Number(before) - sendingAmount) {
+    if (parseInt(after.toString(), 10) !== Number(before) - util.config.amount) {
       throw new Error("alice amount error");
     }
 
     // Check escrow balance
-    const escrowAmount = await token.balanceOf(transfer.target)
-    console.log("escrow = ", escrowAmount.toString())
-    if (parseInt(escrowAmount.toString(), 10) !== sendingAmount) {
+    const escrowAfter= await token.balanceOf(transfer.target)
+    console.log("escrow after = ", escrowAfter.toString())
+    if (parseInt(escrowAfter.toString(), 10) !== Number(escrowBefore) + util.config.amount) {
       throw new Error("escrow amount error");
     }
 
