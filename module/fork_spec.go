5fb6e9a8b5aa2f46dbbd4e85efb1c3f0b1641f71
Naohiro Yoshida <naohiro.yoshida@datachain.jp>
2025-11-06 08:08:21 +0900

    add fork spec
    Signed-off-by: Naohiro Yoshida <naohiro.yoshida@datachain.jp>

--- module/fork_spec.go
+++ module/fork_spec.go
@@ -39,6 +39,7 @@ const (
 	indexPascalHF  = 0
 	indexLorentzHF = 1
 	indexMaxwellHF = 2
+	indexFermiHF   = 3
 )
 
 func getForkSpecParams() []*ForkSpec {
@@ -50,6 +51,7 @@ func getForkSpecParams() []*ForkSpec {
 			MaxTurnLength:             9,
 			GasLimitBoundDivider:      256,
 			EnableHeaderMsec:          false,
+			KAncestorGenerationDepth:  1,
 		},
 		// Lorentz HF
 		{
@@ -58,6 +60,7 @@ func getForkSpecParams() []*ForkSpec {
 			MaxTurnLength:             64,
 			GasLimitBoundDivider:      1024,
 			EnableHeaderMsec:          true,
+			KAncestorGenerationDepth:  1,
 		},
 		// Maxwell HF
 		{
@@ -66,6 +69,16 @@ func getForkSpecParams() []*ForkSpec {
 			MaxTurnLength:             64,
 			GasLimitBoundDivider:      1024,
 			EnableHeaderMsec:          true,
+			KAncestorGenerationDepth:  1,
+		},
+		// Fermi HF
+		{
+			AdditionalHeaderItemCount: 1,
+			EpochLength:               1000,
+			MaxTurnLength:             64,
+			GasLimitBoundDivider:      1024,
+			EnableHeaderMsec:          true,
+			KAncestorGenerationDepth:  3,
 		},
 	}
 }
@@ -76,12 +89,15 @@ func GetForkParameters(network Network) []*ForkSpec {
 	case Localnet:
 		hardForks[indexPascalHF].HeightOrTimestamp = &ForkSpec_Height{Height: 0}
 		hardForks[indexLorentzHF].HeightOrTimestamp = &ForkSpec_Height{Height: 1}
-		hardForks[indexMaxwellHF].HeightOrTimestamp = localLatestHF
+		hardForks[indexMaxwellHF].HeightOrTimestamp = &ForkSpec_Height{Height: 2}
+		hardForks[indexFermiHF].HeightOrTimestamp = localLatestHF
 		return hardForks
 	case Testnet:
 		hardForks[indexPascalHF].HeightOrTimestamp = &ForkSpec_Height{Height: 48576786}
 		hardForks[indexLorentzHF].HeightOrTimestamp = &ForkSpec_Height{Height: 49791365}
 		hardForks[indexMaxwellHF].HeightOrTimestamp = &ForkSpec_Height{Height: 52552978}
+		// TODO Set Fixed height after Fermi HF
+		hardForks[indexFermiHF].HeightOrTimestamp = &ForkSpec_Timestamp{Timestamp: 1762741500_000}
 		return hardForks
 	case Mainnet:
 		hardForks[indexPascalHF].HeightOrTimestamp = &ForkSpec_Height{Height: 47618307}
@@ -91,6 +107,7 @@ func GetForkParameters(network Network) []*ForkSpec {
 		// https://bscscan.com/block/52337091
 		// https://github.com/bnb-chain/bsc/releases/tag/v1.5.16
 		hardForks[indexMaxwellHF].HeightOrTimestamp = &ForkSpec_Height{Height: 52337091}
+		// TODO
 		return hardForks
 	}
 	return nil
--- module/header_query.go
+++ module/header_query.go
@@ -11,7 +11,7 @@ import (
 
 type getHeaderFn func(context.Context, uint64) (*types.Header, error)
 
-func queryLatestFinalizedHeader(ctx context.Context, getHeader getHeaderFn, latestBlockNumber uint64) (uint64, []*ETHHeader, error) {
+func queryLatestFinalizedHeader(ctx context.Context, getHeader getHeaderFn, latestBlockNumber uint64, forkSpecs []*ForkSpec) (uint64, []*ETHHeader, error) {
 	logger := log.GetLogger()
 	for i := latestBlockNumber; i > 0; i-- {
 		header, err := getHeader(ctx, i)
@@ -29,7 +29,7 @@ func queryLatestFinalizedHeader(ctx context.Context, getHeader getHeaderFn, late
 
 		logger.DebugContext(ctx, "Try to seek verifying headers to finalize", "probablyFinalized", probablyFinalized, "latest", latestBlockNumber)
 
-		headers, err := queryFinalizedHeader(ctx, getHeader, probablyFinalized, latestBlockNumber)
+		headers, err := queryFinalizedHeader(ctx, getHeader, probablyFinalized, latestBlockNumber, forkSpecs)
 		if err != nil {
 			return 0, nil, err
 		}
@@ -41,31 +41,48 @@ func queryLatestFinalizedHeader(ctx context.Context, getHeader getHeaderFn, late
 	return 0, nil, fmt.Errorf("no finalized header found: %d", latestBlockNumber)
 }
 
-func queryFinalizedHeader(ctx context.Context, fn getHeaderFn, height uint64, limitHeight uint64) ([]*ETHHeader, error) {
+func queryFinalizedHeader(ctx context.Context, fn getHeaderFn, height uint64, limitHeight uint64, forkSpecs []*ForkSpec) ([]*ETHHeader, error) {
 	var ethHeaders []*ETHHeader
 	for i := height; i+2 <= limitHeight; i++ {
 		targetBlock, targetETHHeader, _, err := queryETHHeader(ctx, fn, i)
 		if err != nil {
 			return nil, err
 		}
-		childBlock, childETHHeader, childVote, err := queryETHHeader(ctx, fn, i+1)
-		if err != nil {
-			return nil, err
-		}
-		_, grandChildETHHeader, grandChildVote, err := queryETHHeader(ctx, fn, i+2)
+		currentForkSpec, _, err := FindTargetForkSpec(forkSpecs, targetBlock.Number.Uint64(), MilliTimestamp(targetBlock))
 		if err != nil {
 			return nil, err
 		}
 
-		if childVote == nil || grandChildVote == nil ||
-			grandChildVote.Data.SourceNumber != targetBlock.Number.Uint64() ||
-			grandChildVote.Data.SourceNumber != childVote.Data.TargetNumber ||
-			grandChildVote.Data.TargetNumber != childBlock.Number.Uint64() {
-			// Append to verify header sequence
-			ethHeaders = append(ethHeaders, targetETHHeader)
-			continue
+		childBlock, childETHHeader, childVote, err := queryETHHeader(ctx, fn, i+1)
+		if err != nil {
+			return nil, err
 		}
-		return append(ethHeaders, targetETHHeader, childETHHeader, grandChildETHHeader), nil
+		var descendants []*ETHHeader
+		for j := uint64(0); j < uint64(currentForkSpec.KAncestorGenerationDepth); j++ {
+			descendantIndex := i + 2 + j
+			if descendantIndex > limitHeight {
+				log.GetLogger().DebugContext(ctx, "Insufficient verifying headers to finalize. no valid descendant found for target.", "height", height, "limit", limitHeight, "target", targetBlock.Number)
+				return nil, nil
+			}
+
+			_, descendantHeader, descendantVote, err := queryETHHeader(ctx, fn, descendantIndex)
+			if err != nil {
+				return nil, err
+			}
+
+			// Ensure valida vote relation
+			if childVote == nil || descendantVote == nil ||
+				descendantVote.Data.SourceNumber != targetBlock.Number.Uint64() ||
+				descendantVote.Data.SourceNumber != childVote.Data.TargetNumber ||
+				descendantVote.Data.TargetNumber != childBlock.Number.Uint64() {
+				// Append to verify header sequence
+				descendants = append(ethHeaders, descendantHeader)
+				continue
+			}
+			return append(append(ethHeaders, targetETHHeader, childETHHeader), descendants...), nil
+		}
+		// Append to verify header sequence
+		ethHeaders = append(ethHeaders, targetETHHeader)
 	}
 	log.GetLogger().DebugContext(ctx, "Insufficient verifying headers to finalize", "height", height, "limit", limitHeight)
 	return nil, nil
