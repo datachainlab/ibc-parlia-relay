3d33e1df41e39a3678b781b3013a129647379157
Naohiro Yoshida <naohiro.yoshida@datachain.jp>
2025-11-06 16:40:24 +0900

    add test

--- module/header_query_test.go
+++ module/header_query_test.go
@@ -14,7 +14,7 @@ import (
 
 type HeaderQueryTestSuite struct {
 	suite.Suite
-	forkSpecs []*ForkSpec
+	forkSpecsPatterns [][]*ForkSpec
 }
 
 func TestHeaderQueryTestSuite(t *testing.T) {
@@ -24,11 +24,15 @@ func TestHeaderQueryTestSuite(t *testing.T) {
 func (ts *HeaderQueryTestSuite) SetupTest() {
 	err := log.InitLogger("DEBUG", "text", "stdout", false)
 	ts.Require().NoError(err)
-	ts.forkSpecs = []*ForkSpec{
-		{
+	ts.forkSpecsPatterns = [][]*ForkSpec{
+		{{
 			HeightOrTimestamp:        &ForkSpec_Height{Height: 0},
 			KAncestorGenerationDepth: 1,
-		},
+		}},
+		{{
+			HeightOrTimestamp:        &ForkSpec_Height{Height: 0},
+			KAncestorGenerationDepth: 3,
+		}},
 	}
 }
 
@@ -41,22 +45,25 @@ func (ts *HeaderQueryTestSuite) TestErrorQueryFinalizedHeader() {
 		}, nil
 	}
 
-	// No finalized header found
-	headers, err := queryFinalizedHeader(context.Background(), fn, 1, 10, ts.forkSpecs)
-	ts.Require().NoError(err)
-	ts.Require().Nil(headers)
+	for _, forkSpecs := range ts.forkSpecsPatterns {
+		// No finalized header found
+		headers, err := queryFinalizedHeader(context.Background(), fn, 1, 10, forkSpecs)
+		ts.Require().NoError(err)
+		ts.Require().Nil(headers)
 
-	fn = func(ctx context.Context, height uint64) (*types.Header, error) {
-		h := headerByHeight(int64(height))
-		if h != nil {
-			return h, nil
+		fn = func(ctx context.Context, height uint64) (*types.Header, error) {
+			h := headerByHeight(int64(height))
+			if h != nil {
+				return h, nil
+			}
+			return &types.Header{Number: big.NewInt(int64(height))}, nil
 		}
-		return &types.Header{Number: big.NewInt(int64(height))}, nil
+
+		headers, err = queryFinalizedHeader(context.Background(), fn, 760, 1000, forkSpecs)
+		ts.Require().NoError(err)
+		ts.Require().Nil(headers)
 	}
 
-	headers, err = queryFinalizedHeader(context.Background(), fn, 760, 1000, ts.forkSpecs)
-	ts.Require().NoError(err)
-	ts.Require().Nil(headers)
 }
 
 func (ts *HeaderQueryTestSuite) TestSuccessQueryFinalizedHeader() {
@@ -69,14 +76,16 @@ func (ts *HeaderQueryTestSuite) TestSuccessQueryFinalizedHeader() {
 		return &types.Header{Number: big.NewInt(int64(height))}, nil
 	}
 
-	headers, err := queryFinalizedHeader(context.Background(), fn, 760, 1002, ts.forkSpecs)
-	ts.Require().NoError(err)
-	ts.Require().Len(headers, 1002-760, len(headers))
+	for _, forkSpecs := range ts.forkSpecsPatterns {
+		headers, err := queryFinalizedHeader(context.Background(), fn, 760, 1002, forkSpecs)
+		ts.Require().NoError(err)
+		ts.Require().Len(headers, 1002-760, len(headers))
+	}
 }
 
 func (ts *HeaderQueryTestSuite) TestSuccessQueryLatestFinalizedHeader() {
 
-	verify := func(latestBlockNumber uint64) {
+	verify := func(latestBlockNumber uint64, forkSpecs []*ForkSpec) {
 		getHeader := func(ctx context.Context, height uint64) (*types.Header, error) {
 			h := headerByHeight(int64(height))
 			if h != nil {
@@ -84,31 +93,35 @@ func (ts *HeaderQueryTestSuite) TestSuccessQueryLatestFinalizedHeader() {
 			}
 			return &types.Header{Number: big.NewInt(int64(height))}, nil
 		}
-		height, h, err := queryLatestFinalizedHeader(context.Background(), getHeader, latestBlockNumber, ts.forkSpecs)
+		height, h, err := queryLatestFinalizedHeader(context.Background(), getHeader, latestBlockNumber, forkSpecs)
 		ts.Require().NoError(err)
 		ts.Require().Len(h, 3)
 		ts.Require().Equal(int(height), 1001)
 	}
-	for i := 1003; i < 1003+100; i++ {
-		verify(uint64(i))
+	for _, forkSpecs := range ts.forkSpecsPatterns {
+		for i := 1003; i < 1003+100; i++ {
+			verify(uint64(i), forkSpecs)
+		}
 	}
 }
 
 func (ts *HeaderQueryTestSuite) TestErrorQueryLatestFinalizedHeader_NoVote() {
 
-	verify := func(latestBlockNumber uint64, extra []byte) {
+	verify := func(latestBlockNumber uint64, extra []byte, forkSpecs []*ForkSpec) {
 		getHeader := func(ctx context.Context, height uint64) (*types.Header, error) {
 			return &types.Header{
 				Number: big.NewInt(int64(height)),
 				Extra:  extra,
 			}, nil
 		}
-		_, _, err := queryLatestFinalizedHeader(context.Background(), getHeader, latestBlockNumber, ts.forkSpecs)
+		_, _, err := queryLatestFinalizedHeader(context.Background(), getHeader, latestBlockNumber, forkSpecs)
 		ts.Require().True(strings.Contains(err.Error(), "no finalized header found"))
 	}
 
-	for i := 0; i < 100; i++ {
-		verify(uint64(i), make([]byte, 0))
-		verify(uint64(i), common.Hex2Bytes("d88301020b846765746888676f312e32302e35856c696e7578000000b19df4a2f8b5831defffb860a44482b16993815ff4903016ce83ef788b455e2c80ba9976e8e55ac6591b9f9965234a0a2c579269bc5e09577977322d07d17bb8d657ac621a1abfadcb35b9c9d4713dbdd3d47fd3cc6dc2475c989aa224fecd083101049ef1adea2718b00e37f84c8401e5c5cfa0be938dfeafe5b932c2dcef0e2bebb1a05f31104a59b49d78b0b7746a483c14648401e5c5d0a03658f0bb6692995a9dd3b72a69ec6e8e1b9af4361718d8a275c2b92d26eeffc28027cb6d065d5a6d8749ca45a185add61b9ce470136898643170f8072513ca45f35d826f02cb2494f857beebdac9ec04196c8b30a65352ef155a28ac6a0057ff1601"))
+	for _, forkSpecs := range ts.forkSpecsPatterns {
+		for i := 0; i < 100; i++ {
+			verify(uint64(i), make([]byte, 0), forkSpecs)
+			verify(uint64(i), common.Hex2Bytes("d88301020b846765746888676f312e32302e35856c696e7578000000b19df4a2f8b5831defffb860a44482b16993815ff4903016ce83ef788b455e2c80ba9976e8e55ac6591b9f9965234a0a2c579269bc5e09577977322d07d17bb8d657ac621a1abfadcb35b9c9d4713dbdd3d47fd3cc6dc2475c989aa224fecd083101049ef1adea2718b00e37f84c8401e5c5cfa0be938dfeafe5b932c2dcef0e2bebb1a05f31104a59b49d78b0b7746a483c14648401e5c5d0a03658f0bb6692995a9dd3b72a69ec6e8e1b9af4361718d8a275c2b92d26eeffc28027cb6d065d5a6d8749ca45a185add61b9ce470136898643170f8072513ca45f35d826f02cb2494f857beebdac9ec04196c8b30a65352ef155a28ac6a0057ff1601"), forkSpecs)
+		}
 	}
 }
